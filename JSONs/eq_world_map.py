6989f995728574067a5fd17c135cf3bd7b648ef2
gatomario <>
2021-08-16 21:15:19 -0700

    add scripts to manipulate json files and its information, in this case geological info

---
+++ JSONs/eq_world_map.py
@@ -0,0 +1,37 @@
+import json
+from plotly.graph_objects import Scattergeo, Layout
+from plotly import offline
+
+"""making a list of all magnitudes"""
+filename = '/home/gato/SCRIPTS_001/JSONs/4.5_day.geojson'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+## map the terremotos
+
+data = [{
+    'type':'scattergeo',
+    'lon':lons, 
+    'lat':lats, 
+    'marker':{
+        'size':[5*mag for mag in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar': {'title':'Magnitud'}
+
+            },
+}]
+my_layout = Layout(title="Los Terremotos")
+fig = {'data':data, 'layout':my_layout}
+offline.plot(fig, filename='terremotos.html')
